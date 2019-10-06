9e673f420ac509ebbc8bfd5e8280876b20525c6d
Maxwell Schwartz <schwartzm1231@gmail.com>
2019-10-06 14:02:11 -0400

    Walk cycle

---
+++ Start with Nothing/Assets/Scripts/Interactable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public GameObject sprite;
+    void Start()
+    {
+        sprite.GetComponent<SpriteRenderer>();
+        // sprite.disabled;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void interactedWith()
+    {
+        // sprite.enabled;
+    }
+}
